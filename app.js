0ff6539c4af6d13eb6c949a93b47280031362aa3
DavidPinerosC <david.pineros@sainapsis.com>
2025-02-22 16:13:11 -0500

    authentication

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const app = express();
+app.use(express.json());
 
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
 
+const users = [
+    { user: "leo"},
+    { user: "david" },
+  ];
 
 app.use((req, res, next) => {
     const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
@@ -11,6 +20,37 @@ app.use((req, res, next) => {
         return res.status(405).json({ error: 'Método HTTP no permitido.' });
     }
     next();
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const user = users.find((user) => user.user === username);
+    if (!user) {
+      res.status(401).send({ error: "Invalid user name or password" });
+    }
+
+    const token = jwt.sign(
+      {
+        username: username,
+      },
+      process.env.SECRET_KEY);
+  
+    res.send({ token });
+});
+
+function JWTValidation(req, res, next) {
+    const token = req.headers.authorization;
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.username = decoded.username;
+        next();
+    } catch (err) {
+        res.status(401).send({ error: "No tienes permisos para acceder a este recurso" });
+    }
+}
+
+app.get("/leoPrivateDirectory", JWTValidation, (req, res) => {
+    res.send({ message: "Este es el directorio privado de Leo" });
 });
 
 app.use('/list', listViewRouter);
